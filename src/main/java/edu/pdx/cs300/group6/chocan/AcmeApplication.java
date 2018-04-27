4420ee605b30f5a275a4675486ff5ec7fb931a61
Bradley Odell <btodell@hotmail.com>
2018-04-27 00:29:59 -0700

    Initial commit with boilerplate Maven config (pom.xml).

---
+++ src/main/java/edu/pdx/cs300/group6/chocan/AcmeApplication.java
@@ -0,0 +1,16 @@
+package edu.pdx.cs300.group6.chocan;
+
+/**
+ *
+ */
+public class AcmeApplication {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        // TODO
+    }
+
+}
---
+++ src/main/java/edu/pdx/cs300/group6/chocan/ProvidersApplication.java
@@ -0,0 +1,16 @@
+package edu.pdx.cs300.group6.chocan;
+
+/**
+ *
+ */
+public class ProvidersApplication {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        // TODO
+    }
+
+}
