2a68db4d2e52ef2b1601a4ca6ab2b0c79f949f2a
Bradley Odell <btodell@hotmail.com>
2018-05-13 16:17:49 -0700

    Implemented provider report generation. Improved documentation for service record database.

--- src/main/java/chocan/report/ReportGenerator.java
+++ src/main/java/chocan/report/ReportGenerator.java
@@ -8,8 +8,10 @@ import chocan.users.*;
 import chocan.utils.DateUtils;
 import chocan.utils.StringUtils;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,6 +21,9 @@ import java.util.stream.Collectors;
 public class ReportGenerator {
 
     private static final String DATE_FORMAT = "MM-dd-uuuu";
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    private static final String DATE_TIME_FORMAT = "MM-dd-uuuu HH:mm:ss";
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
 
     private final IUserDatabase<Member> memberDatabase;
     private final IUserDatabase<Provider> providerDatabase;
@@ -51,8 +56,8 @@ public class ReportGenerator {
         final StringBuilder sb = new StringBuilder();
         // Print member information
         sb.append("Name: ").append(member.name).append('\n');
-        sb.append("ID: ").append(member.id).append('\n');
-        sb.append("Street Address: ").append(member.address).append('\n');
+        sb.append("Account number: ").append(member.id).append('\n');
+        sb.append("Street address: ").append(member.address).append('\n');
         sb.append("City: ").append(member.city).append('\n');
         sb.append("State: ").append(member.state).append('\n');
         sb.append("Zip: ").append(member.zip).append('\n');
@@ -62,6 +67,7 @@ public class ReportGenerator {
         final List<ServiceRecord> memberServiceRecords = this.serviceRecordDatabase
                 .get(weekRange[0], weekRange[1])
                 .filter(record -> record.memberID == member.id)
+                .sorted(Comparator.comparing(record1 -> record1.serviceDate))
                 .collect(Collectors.toList());
         // Print service record information
         final int idStringLength = Integer.toString(memberServiceRecords.size()).length();
@@ -69,9 +75,10 @@ public class ReportGenerator {
         for (final ServiceRecord record : memberServiceRecords) {
             final Service service = this.providerServiceDirectory.get(record.serviceCode);
             final Provider provider = this.providerDatabase.get(record.providerID);
+            final String padding = StringUtils.padLeft("", idStringLength);
             sb.append(StringUtils.padLeft(Integer.toString(i), idStringLength)).append(". ").append(service != null ? service.name : "Unknown service").append('\n');
-            sb.append(StringUtils.padLeft("", idStringLength)).append("| by ").append(provider != null ? provider.name : "Unknown provider").append('\n');
-            sb.append(StringUtils.padLeft("", idStringLength)).append("` on ").append(record.serviceDate.format(DateTimeFormatter.ofPattern(DATE_FORMAT)));
+            sb.append(padding).append("| by ").append(provider != null ? provider.name : "Unknown provider").append('\n');
+            sb.append(padding).append("` on ").append(record.serviceDate.format(DATE_FORMATTER)).append('\n');
             i++;
         }
         // Return finished member report
@@ -79,22 +86,63 @@ public class ReportGenerator {
     }
 
     /**
-     *
-     * @param provider
-     * @return
+     * Generates a provider's report for the week.
+     * @param provider The provider to generate the report for.
+     * @return A string containing the report contents.
      */
     public String forProvider(final Provider provider) {
         final StringBuilder sb = new StringBuilder();
-        // TODO
+        // Print provider information
+        sb.append("Name: ").append(provider.name).append('\n');
+        sb.append("Account number: ").append(provider.id).append('\n');
+        sb.append("Street address: ").append(provider.address).append('\n');
+        sb.append("City: ").append(provider.city).append('\n');
+        sb.append("State: ").append(provider.state).append('\n');
+        sb.append("Zip: ").append(provider.zip).append('\n');
+        // Get current week date time range
+        final LocalDateTime[] weekRange = DateUtils.getWeekRange();
+        // Query for recorded services provided
+        final List<ServiceRecord> providerServiceRecords = this.serviceRecordDatabase
+                .get(weekRange[0], weekRange[1])
+                .filter(record -> record.providerID == provider.id)
+                .sorted(Comparator.comparing(record1 -> record1.serviceDate))
+                .collect(Collectors.toList());
+        // Print service record information
+        final int idStringLength = Integer.toString(providerServiceRecords.size()).length();
+        int i = 1;
+        BigDecimal totalFee = BigDecimal.ZERO;
+        for (final ServiceRecord record : providerServiceRecords) {
+            final Service service = this.providerServiceDirectory.get(record.serviceCode);
+            final Member member = this.memberDatabase.get(record.memberID);
+            final String padding = StringUtils.padLeft("", idStringLength);
+            sb.append(StringUtils.padLeft(Integer.toString(i), idStringLength)).append(". ").append(service != null ? service.name : "Unknown service").append('\n');
+            sb.append(padding).append("| Date: ").append(record.serviceDate.format(DATE_FORMATTER)).append('\n');
+            sb.append(padding).append("| Date received: ").append(record.dateTime.format(DATE_TIME_FORMATTER)).append('\n');
+            sb.append(padding).append("| Member name: ").append(member != null ? member.name : "Unknown member").append('\n');
+            sb.append(padding).append("| Member number: ").append(record.memberID).append('\n');
+            sb.append(padding).append("| Service code: ").append(record.serviceCode).append('\n');
+            sb.append(padding).append("` Fee: $");
+            if (service != null) {
+                sb.append(service.fee);
+                totalFee = totalFee.add(service.fee);
+            } else {
+                sb.append("0 (Unknown service)");
+            }
+            sb.append('\n');
+            i++;
+        }
+        // Print totals
+        sb.append("Total consultations provided: ").append(providerServiceRecords.size()).append('\n');
+        sb.append("Total fee for the week: $").append(totalFee).append('\n');
+        // Return finished provider report
         return sb.toString();
     }
 
     /**
-     *
-     * @param manager
-     * @return
+     * Generates a manager's summary report for the week.
+     * @return A string containing the report contents.
      */
-    public String forManager(final Manager manager) {
+    public String forManager() {
         final StringBuilder sb = new StringBuilder();
         // TODO
         return sb.toString();
--- src/main/java/chocan/service/IServiceRecordDatabase.java
+++ src/main/java/chocan/service/IServiceRecordDatabase.java
@@ -4,15 +4,17 @@ import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
 /**
- *
+ * A read-only interface for the service record database.
  */
 public interface IServiceRecordDatabase {
 
     /**
-     *
-     * @param from
-     * @param to
-     * @return
+     * Gets all the service records that were recorded between the two given date times.
+     * The date time range is based on the date time the records were created (and received by the system).
+     * @see ServiceRecord#dateTime
+     * @param from The lower bound of the date time range.
+     * @param to The upper bound of the date time range.
+     * @return A stream of service records.
      */
     Stream<ServiceRecord> get(final LocalDateTime from, final LocalDateTime to);
 
