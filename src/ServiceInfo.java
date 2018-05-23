6bde1e836a86fb0f25546b1541b88c9c4c733042
bbelenpdx <bbelenpdx@users.noreply.github.com>
2018-05-22 19:07:16 -0700

    added functions and data members

--- src/ServiceInfo.java
+++ src/ServiceInfo.java
@@ -1,5 +1,36 @@
 //ServiceInfo: holds info for each service code. Contained within Provider Directory
 
 public class ServiceInfo {
+    //Data fields
+    private String name;
+    private Integer code;
+
+    //Default constructor
+    public ServiceInfo() {
+        this.name = null;
+        this.code = 0;
+    }
+
+    //Copy constructor
+    public ServiceInfo(ServiceInfo to_copy) {
+        this.name = to_copy.name;
+        this.code = to_copy.code;
+    }
+
+    //Copy constructor w/ args
+    public ServiceInfo(String name, Integer code){
+        this.name = name;
+        this.code = code;
+    }
+
+    //Display function
+    public void Display() {
+        System.out.println("SERVICE INFO: ");
+        System.out.println("Service name: " + name);
+        System.out.println("Service code: " + code);
+    }
+
+
+
 
 }
