3dacf45ab80697d91fd4c2b07af27652f612ff89
Bounnoy Phanthavong <bounnoy@bounnoy.com>
2018-05-24 04:09:51 -0700

    Added tests classes.

---
+++ src/test/java/chocan/ProviderDBTest.java
@@ -0,0 +1,33 @@
+package chocan;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProviderDBTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void checkID() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void load() {
+    }
+
+    @Test
+    public void save() {
+    }
+}
