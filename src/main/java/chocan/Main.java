1144af64c600a91deffc4cebc934c52fe644f4c7
Nick Battalia <battalia@pdx.edu>
2018-05-23 20:37:09 -0700

    Added notes to main

--- src/main/java/chocan/Main.java
+++ src/main/java/chocan/Main.java
@@ -6,11 +6,12 @@ public class Main {
         // TODO User menu needs to be built.
 
         ProviderDir tree = new ProviderDir();
+
+        //tree.Add();//Testing, This prompts user for input to build the provide dir tree.
         //tree.Add();
         //tree.Add();
-        //tree.Add();
-        tree.Read_txt();
-        tree.Display();
+        tree.Read_txt();//Testing, this reads from the provider dir text file and builds directory tree
+        tree.Display();//Test, this displays the provider directory tree alphabetically by name.
 
 
 
