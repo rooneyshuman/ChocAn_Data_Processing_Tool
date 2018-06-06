0ed9faa83c73de137e415c647d08a630a625f424
Nick Battalia <battalia@pdx.edu>
2018-06-06 04:44:25 -0700

    started a date object converter

--- src/main/java/chocan/MemberService.java
+++ src/main/java/chocan/MemberService.java
@@ -1,6 +1,9 @@
 package chocan;
 
+import javafx.util.converter.DateStringConverter;
+
 import java.io.PrintWriter;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import static java.lang.System.out;
@@ -13,8 +16,7 @@ public class MemberService {
     private MemberService next;
 
     //Constructor
-    MemberService()
-    {
+    MemberService() {
         providerName = null;
         service = null;
         next = null;
@@ -31,48 +33,67 @@ public class MemberService {
         write.println(this.service);
     }
 
-    public void delete(){
+    public void delete() {
         providerName = null;
         service = null;
         next = null;
         serviceDate = null;
     }
 
-    public MemberService goNext(){
+    public MemberService goNext() {
         return next;
     }
 
-    public void setNext(MemberService next){
+    public void setNext(MemberService next) {
         this.next = next;
     }
 
     //Used when loading from a file for the first time
-    MemberService(String date, String ProvName, String serviceName){
+    MemberService(String date, String ProvName, String serviceName) {
         serviceDate = date;
         providerName = ProvName;
         service = serviceName;
     }
 
     //Adds service info manually for a member.
-    public void addService(String serviceDate, String providerName, String serviceName){
+    public void addService(String serviceDate, String providerName, String serviceName) {
         this.service = serviceName;
         this.providerName = providerName;
         this.serviceDate = serviceDate;
+
     }
 
     //Displays when member's name is displayed
-    public void display(){
+    public void display() {
         out.println("Date of Service: " + serviceDate);
         out.println("Service Received: " + service);
         out.println("Provider: " + providerName + '\n');
     }
 
-    public boolean compareDate(String toCompare){
+    public boolean compareDate(String toCompare) {
         return serviceDate.equals(toCompare);
     }
 
+    public void Comparedate(String Servicedate) {
+
+
+        try
+
+        {
+            Date date1 = new SimpleDateFormat("MM-DD-YYYY").parse(Servicedate);
+            Date date2 = new SimpleDateFormat("MM-DD-YYYY").parse(this.serviceDate);
+
+        } catch (
+                ParseException e)
+
+        {
+            e.printStackTrace();
+        }
+
+    }
     //Returns current date formatted
     private String getDate(){
+
         String pattern = "MM-dd-yyyy";
         SimpleDateFormat formatDate = new SimpleDateFormat(pattern);
         Date currentDate = new Date();
